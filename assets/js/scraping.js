ba0b5482e8217569c96ad8bbd471bec393934517
Alejandro C. Grant <authorcgrant@gmail.com>
2021-12-06 04:41:37 -0800

    effective file append, trying to find iterative approach that maintains JSON structure

--- assets/js/scraping.js
+++ assets/js/scraping.js
@@ -1,23 +1,34 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const pretty = require('pretty');
 
-const url = 'https://arcade-academy-1.herokuapp.com/'
+const proj_urls = [
+    'https://arcade-academy-1.herokuapp.com/',
+    'https://erudite-1.herokuapp.com/',
+    'https://finstagram-1.herokuapp.com/',
+    'https://creepy-crawler-1.herokuapp.com/'
+]
 
-const metaCompiler = (url) => {
-    axios.get(url)
+const metaCompiler = (proj_urls) => {
+    for (let i = 0; i <= proj_urls.length - 1; i++) {
+        axios.get(proj_urls[i])
         .then((res) => {
             const $ = cheerio.load(res.data);
+            const metaObj = {};
             $('head').children('meta').each((idx, meta) => {
-                metaName = 
-                metaObj[meta.attribs.name] = meta.attribs.content
-                console.log(metaObj);
+                metaObj[meta.attribs.name] = meta.attribs.content;
+            })
+            const title = $('title').text();
+            const jsonMetaObj = JSON.stringify(metaObj, null, 4);
+            fs.appendFile('./meta_data.json', `{${'\n'}"${title}": ${jsonMetaObj}${'\n'}}`, (err) => {
+                if (err) throw err;
+                console.log('Data successfully appended :D');
             })
         })
         .catch((err) => {
             console.log(`End of the line error: ${err}`);
         })
+    }
 };
 
-metaCompiler(url);
+metaCompiler(proj_urls);
